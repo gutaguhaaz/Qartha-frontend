fe76a8e0c29e64253ea2f5feb14f4cbe7f51719a
Luis Fernando Gutiérrez Haaz <ferhaaz@gmail.com>
2025-07-07 21:40:25 +0000

    changes

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -16,8 +16,8 @@ export class ConfigService {
       layout: {
         rtl: false, // options:  true & false
         variant: 'light', // options:  light & dark
-        theme_color: 'cyan', // options:  white, black, purple, blue, cyan, green, orange
-        logo_bg_color: 'cyan', // options:  white, black, purple, blue, cyan, green, orange
+        theme_color: 'blue', // options:  white, black, purple, blue, cyan, green, orange
+        logo_bg_color: 'white', // options:  white, black, purple, blue, cyan, green, orange
         sidebar: {
           collapsed: false, // options:  true & false
           backgroundColor: 'light', // options:  light & dark
